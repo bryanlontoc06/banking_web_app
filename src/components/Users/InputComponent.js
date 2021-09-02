27e62f1a180a063105d3c68dab295ae15baccf02
Greg Lobrigo <golivefun@gmail.com>
2021-09-02 15:35:36 +0800

    refactor code

--- src/components/Users/InputComponent.js
+++ src/components/Users/InputComponent.js
@@ -3,8 +3,7 @@ import React from 'react'
 function InputComponent(props) {
     const {inputType, controlType, floatType, placeholderTitle, setAmountToTransfer, amountToTransfer, isLabel, label} = props
     return (
-        <>
-        {/* <input type="number" className="form-control" id="floatingTransfer" placeholder="Transfer" onChange={(e) => setAmountToTransfer(e.target.value)} value={amountToTransfer}/> */}
+        <>        
             <input type={inputType} className={controlType} id={floatType} placeholder={placeholderTitle} onChange={(e) => setAmountToTransfer(e.target.value)} value={amountToTransfer}/>
             {isLabel? <label>₱ {label}</label> : ''} 
         </>
