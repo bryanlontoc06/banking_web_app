95a15e23adc5b97c6f92b14b307be3dfaea35194
Greg Lobrigo <golivefun@gmail.com>
2021-09-14 18:53:36 +0800

    change images in home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,9 @@
-
 import "./style.css"
 import cc from "../../assets/cc.svg"
+import phone from "../../assets/phone.svg"
+import revenue from "../../assets/revenue.svg"
+import security from "../../assets/security.svg"
+import feedback from "../../assets/feedback.svg"
 const Index = () => {    
 
 
@@ -14,11 +17,11 @@ const Index = () => {
         <div className="box-item item1">
 
         <div className="card" style={{width: "100%", height: "100%"}}>
-        <object className="svg" data={cc} width="auto%" height="auto%"> </object>
+        <object className="svg" data={cc} width="auto" height="auto"> </object>
          <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#test" className="btn btn-primary">Go somewhere</a>
+        <h5 className="card-title">Open and manage accounts with ease.</h5>
+        <span style={{display: "flex", justifyContent: "space-between"}}><p className="card-text">No need to drop by a branch.</p><a href="#test" className="btn btn-primary">Get started!</a></span>
+        {/* <p className="card-text">No need to drop by a branch.</p> */}
         </div>
         </div>        
 
@@ -27,11 +30,10 @@ const Index = () => {
         <div className="box-item item2">
 
         <div className="card" style={{width: "100%", height: "100%"}}>
-        <object className="svg" data={cc} width="auto%" height="auto%"> </object>
+        <object className="svg" data={phone} width="auto" height="auto"> </object>
          <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build.</p>
-        <a href="#test" className="btn btn-primary">Go somewhere</a>
+        <h5 className="card-title">Enjoy real-time and hassle-free transfers</h5>
+        <p className="card-text">Transfer money to other banks via Instapay.</p>        
         </div>
         </div>        
         
@@ -40,11 +42,11 @@ const Index = () => {
         <div className="box-item item3">
 
         <div className="card" style={{width: "100%", height: "100%"}}>
-        <object className="svg" data={cc} width="auto%" height="auto%"> </object>
+        <object className="svg" data={revenue} width="auto" height="auto"> </object>
          <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#test" className="btn btn-primary">Go somewhere</a>
+        <h5 className="card-title">Stash away funds for your passions</h5>
+        <p className="card-text">Easily open your second savings account via our web portal.</p>
+        {/* <a href="#test" className="btn btn-primary">Go somewhere</a> */}
         </div>
         </div>  
 
@@ -52,11 +54,11 @@ const Index = () => {
         <div className="box-item item4">
 
         <div className="card" style={{width: "100%", height: "100%"}}>
-        <object className="svg" data={cc} width="auto%" height="auto%"> </object>
+        <object className="svg" data={security} width="auto" height="auto"> </object>
          <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#test" className="btn btn-primary">Go somewhere</a>
+        <h5 className="card-title">Relax and know your money is safe.</h5>
+        <p className="card-text">Your account is safe with our world class security measures.</p>
+        {/* <a href="#test" className="btn btn-primary">Go somewhere</a> */}
         </div>
         </div>  
 
@@ -65,11 +67,11 @@ const Index = () => {
         <div className="box-item item5">
 
         <div className="card" style={{width: "100%", height: "100%"}}>
-        <object className="svg" data={cc} width="auto%" height="auto%"> </object>
+        <object className="svg" data={feedback} width="auto" height="auto"> </object>
          <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#test" className="btn btn-primary">Go somewhere</a>
+        <h5 className="card-title">We're always ready to listen</h5>
+        <p className="card-text">Let us know what you think!</p>
+        {/* <a href="#test" className="btn btn-primary">Go somewhere</a> */}
         </div>
         </div>  
 
